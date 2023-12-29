50b305032e2f89181aa8abdb0a11ee867be5de41
elrogerr <user.email>
2023-12-28 19:22:13 -0600

    a ver si funciona

---
+++ db.py
@@ -0,0 +1,22 @@
+import mysql.connector
+
+midb = mysql.connector.connect(
+    host = 'localhost',
+    user = 'elrogerrr',
+    password = 'zapatanomurio666',
+    database = 'rogerdb'
+)
+
+cursor = midb.cursor()
+
+# cursor.execute('SELECT * FROM Usuario')
+# resultado = cursor.fetchall()
+# print(resultado)
+
+sql = ' INSERT INTO Usuario (nombre,apellido,email) values (%s,%s,%s) '
+values = ('roberto','shnyder','rob@paperclips.com.mx')
+
+cursor.execute(sql,values)
+midb.commit()
+print (cursor.rowcount)
+
