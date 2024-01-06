c255b55611cc5d87fef91df8eacd77fee6abffd7
elrogerrr <elrogerrr@hotmail.com>
2024-01-05 18:11:13 -0600

    priimera de app flask

--- __init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, url_for, request, Response
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-    return 'Hello World'
-
-if __name__ == "__main__":
-    app.run()
---
+++ todo/__init__.py
@@ -0,0 +1,19 @@
+import os
+from flask import Flask
+
+def create_app():
+    app = Flask(__name__)
+    
+    app.config.from_mapping(
+        SECRET_KEY='rogeliosolorzanoescobar',
+        DATABASE_HOST=os.environ.get('FLASK_DATABASE_HOST'),
+        DATABASE_USER=os.environ.get('FLASK_DATABASE_USER'),
+        DATABASE_PASSWORD=os.environ.get('FLASK_DATABASE_PASSWORD'),
+        DATABASE=os.environ.get('FLASK_DATABASE'),
+    )
+    
+    @app.route('/hola')
+    def hola():
+        return 'Hola mundirijillo'
+    
+    return app
